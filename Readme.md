588c09a3f4488b15c7da5aec091d46d88e6b2eee
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,24 @@
+# How to bind SchedulerControl to an SQL Server database and customize Update/Delete/Insert queries without the use of the SqlCommandBuilder
+
+
+<p>This example illustrates how to setup SchedulerControl binding at runtime. Note that all appointment mappings and even SQL queries along with their parameters are adjusted in code without the use of the VS designer. This is the most flexible database-binding technique because it allows you to modify data-binding options and SQL queries with ease. For instance, we keep the time of modification along with the modified row by inserting a <a href="http://msdn.microsoft.com/en-us/library/ms188383.aspx"><u>GETDATE</u></a> function result into a <strong>TimeStamp </strong>column. Here are to corresponding SQL queries:</p>
+
+
+```sql
+INSERT INTO CarScheduling (StartTime, EndTime, Subject, TimeStamp) VALUES (@StartTime, @EndTime, @Subject, GetDate())
+
+UPDATE CarScheduling SET StartTime = @StartTime, EndTime = @EndTime, Subject = @Subject, TimeStamp = GetDate() WHERE ID = @ID
+
+DELETE FROM CarScheduling WHERE ID = @ID
+```
+
+
+<p> </p>
+<p>To test this example locally, setup "SchedulerBindDynamically" sample database in your SQL Server instance. Utilize the SchedulerBindDynamically.sql file attached to this example to generate a sample database and table schema.</p>
+<p><strong>Note:</strong> <br> We do not use the <a href="http://msdn.microsoft.com/en-us/library/system.data.sqlclient.sqlcommandbuilder.aspx"><u>SqlCommandBuilder Class</u></a> capabilities to generate SQL queries as it is shown in the <a href="https://www.devexpress.com/Support/Center/p/E551">How to bind SchedulerControl to MS SQL Server database at runtime</a> code example. The <a href="http://msdn.microsoft.com/en-us/library/system.data.common.dbcommandbuilder.conflictoption.aspx"><u>DbCommandBuilder.ConflictOption Property</u></a> option of this class allows you to enable optimistic concurrency to prevent a concurrency violation error. Since we do not use this functionality, you might encounter this error when using this example. Fortunately, there are some ways that allows you to avoid this error. You can find them in the <a href="http://msdn.microsoft.com/en-us/library/cs6hb8k4.aspx"><u>Introduction to Data Concurrency in ADO.NET</u></a> MSDN article.</p>
+<p><strong>See Also:</strong><br> <a href="http://msdn.microsoft.com/en-us/library/33y2221y.aspx"><u>Updating Data Sources with DataAdapters</u></a></p>
+<p><a href="https://documentation.devexpress.com/WindowsForms/2949/Controls-and-Libraries/Scheduler/Getting-Started">Getting Started</a></p>
+
+<br/>
+
+
---
+++ VB/Form1.vb
@@ -0,0 +1,100 @@
+﻿Imports System
+Imports System.Data
+Imports System.Windows.Forms
+Imports DevExpress.XtraScheduler
+Imports System.Data.SqlClient
+
+Namespace SchedulerBindDynamically
+    Partial Public Class Form1
+        Inherits Form
+
+        Private dataSet As DataSet
+        Private dataAdapter As SqlDataAdapter
+
+        Public Sub New()
+            InitializeComponent()
+
+            BindScheduler()
+        End Sub
+
+        Private Sub BindScheduler()
+            ' 1) Retrieve data rows
+            Dim connection As New SqlConnection("Data Source=.\SQLExpress;Initial Catalog=SchedulerBindDynamically;Integrated Security=SSPI")
+            Dim selectCommand As New SqlCommand("SELECT ID, StartTime, EndTime, Subject FROM CarScheduling", connection)
+
+            dataSet = New DataSet()
+            dataAdapter = New SqlDataAdapter(selectCommand)
+
+            dataAdapter.Fill(dataSet, "CarScheduling")
+
+            ' 2) Adjust mappings
+            Dim schedulerStorage As SchedulerStorage = schedulerControl1.Storage
+            Dim appointmentMappings As AppointmentMappingInfo = schedulerStorage.Appointments.Mappings
+
+            appointmentMappings.AppointmentId = "ID"
+            appointmentMappings.Start = "StartTime"
+            appointmentMappings.End = "EndTime"
+            appointmentMappings.Subject = "Subject"
+
+            schedulerStorage.Appointments.CommitIdToDataSource = False
+
+            ' 3) Bind scheduler to data
+            schedulerStorage.Appointments.DataSource = dataSet.Tables("CarScheduling")
+            If schedulerStorage.Appointments.Count > 0 Then
+                schedulerControl1.Start = schedulerStorage.Appointments(0).Start
+            End If
+
+            ' 4) Define Insert, Update, Delete commands
+            dataAdapter.InsertCommand = New SqlCommand("INSERT INTO CarScheduling (StartTime, EndTime, Subject, TimeStamp) VALUES (@StartTime, @EndTime, @Subject, GetDate())", connection)
+
+            dataAdapter.InsertCommand.Parameters.Add("@StartTime", SqlDbType.DateTime)
+            dataAdapter.InsertCommand.Parameters.Add("@EndTime", SqlDbType.DateTime)
+            dataAdapter.InsertCommand.Parameters.Add("@Subject", SqlDbType.NVarChar)
+
+            dataAdapter.InsertCommand.Parameters("@StartTime").SourceColumn = "StartTime"
+            dataAdapter.InsertCommand.Parameters("@EndTime").SourceColumn = "EndTime"
+            dataAdapter.InsertCommand.Parameters("@Subject").SourceColumn = "Subject"
+
+            dataAdapter.UpdateCommand = New SqlCommand("UPDATE CarScheduling SET StartTime = @StartTime, EndTime = @EndTime, Subject = @Subject, TimeStamp = GetDate() WHERE ID = @ID", connection)
+
+            dataAdapter.UpdateCommand.Parameters.Add("@ID", SqlDbType.Int)
+            dataAdapter.UpdateCommand.Parameters.Add("@StartTime", SqlDbType.DateTime)
+            dataAdapter.UpdateCommand.Parameters.Add("@EndTime", SqlDbType.DateTime)
+            dataAdapter.UpdateCommand.Parameters.Add("@Subject", SqlDbType.NVarChar)
+
+            dataAdapter.UpdateCommand.Parameters("@ID").SourceColumn = "ID"
+            dataAdapter.UpdateCommand.Parameters("@StartTime").SourceColumn = "StartTime"
+            dataAdapter.UpdateCommand.Parameters("@EndTime").SourceColumn = "EndTime"
+            dataAdapter.UpdateCommand.Parameters("@Subject").SourceColumn = "Subject"
+
+            dataAdapter.DeleteCommand = New SqlCommand("DELETE FROM CarScheduling WHERE ID = @ID", connection)
+            dataAdapter.DeleteCommand.Parameters.Add("@ID", SqlDbType.Int)
+            dataAdapter.DeleteCommand.Parameters("@ID").SourceColumn = "ID"
+
+            ' 5) Subscribe to events (data-related operations)
+            AddHandler schedulerStorage.AppointmentsInserted, AddressOf Storage_AppointmentsModified
+            AddHandler schedulerStorage.AppointmentsChanged, AddressOf Storage_AppointmentsModified
+            AddHandler schedulerStorage.AppointmentsDeleted, AddressOf Storage_AppointmentsModified
+            AddHandler dataAdapter.RowUpdated, AddressOf Adapter_RowUpdated
+        End Sub
+
+        Private Sub Storage_AppointmentsModified(ByVal sender As Object, ByVal e As PersistentObjectsEventArgs)
+            Try
+                dataAdapter.Update(dataSet.Tables("CarScheduling"))
+            Catch ex As DBConcurrencyException
+                MessageBox.Show("Concurrency violation:" & ControlChars.CrLf & ex.Row("Subject").ToString())
+            End Try
+            'dataSet.AcceptChanges();
+        End Sub
+
+        Private Sub Adapter_RowUpdated(ByVal sender As Object, ByVal e As System.Data.SqlClient.SqlRowUpdatedEventArgs)
+            If e.Status = UpdateStatus.Continue AndAlso e.StatementType = StatementType.Insert Then
+                Dim id As Integer = 0
+                Using cmd As New SqlCommand("SELECT IDENT_CURRENT('CarScheduling')", dataAdapter.SelectCommand.Connection)
+                    id = Convert.ToInt32(cmd.ExecuteScalar())
+                End Using
+                e.Row("ID") = id
+            End If
+        End Sub
+    End Class
+End Namespace
